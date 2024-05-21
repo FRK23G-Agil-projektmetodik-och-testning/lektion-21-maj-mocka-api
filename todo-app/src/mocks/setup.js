e57c173c7131f7aa08c871d67367d3717490f64d
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-05-21 14:03:14 +0200

    Added code

--- todo-app/src/mocks/setup.js
+++ todo-app/src/mocks/setup.js
@@ -1,14 +1,4 @@
 import { setupServer } from 'msw/node';
 import { handlers } from './handlers';
 
-const server = setupServer(...handlers);
-
-// Innan alla tester körs så starta och lyssna på servern
-beforeAll(() => {
-  server.listen();
-});
-
-// Efter alla tester körts så stäng ner servern
-afterAll(() => {
-  server.close();
-});
+export const server = setupServer(...handlers);
